95d682aa46b04fdf473eac17fa3d7f98f858a720
Dxde <Dxde@93.74.36.211>
2015-12-05 15:57:55 +0200

    race

---
+++ ThrRace/src/tr/ThreadsMain.java
@@ -0,0 +1,51 @@
+package tr;
+
+public class ThreadsMain {
+	public static void main(String[] args) {
+		Thread threadImplements = new Thread(new ThreadImplements());
+		ThreadExtends threadExtends = new ThreadExtends();
+
+		threadImplements.start();
+		threadExtends.start();
+	}
+}
+
+class ThreadImplements implements Runnable {
+
+	ThreadImplements() {
+	}
+
+	public void run() {
+		int num = 0;
+
+		while (num < 10) {
+			System.out.println("ThreadImplements " + num);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			num++;
+		}
+	}
+}
+
+class ThreadExtends extends Thread {
+
+	ThreadExtends() {
+	}
+
+	public void run() {
+		int num = 0;
+
+		while (num < 10) {
+			System.out.println("ThreadExtends " + num);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			num++;
+		}
+	}
+}
